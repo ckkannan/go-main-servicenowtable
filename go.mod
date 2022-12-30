32b264f8290831bf6c1c4ef26f430b06c3a3cf2e
Kannan Chairman <kannan.chairman@prudential.com>
2022-12-30 11:21:48 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module gt-ss-dp/capamclient
+
+go 1.19
+
+replace gt-ss-dp/capam => ../terraform-module-capam
+
+require gt-ss-dp/capam v0.0.0-00010101000000-000000000000
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"gt-ss-dp/capam"
+	"os"
+)
+
+const HostURL string = "https://p1ehowld170.prudential.com"
+
+var h string = HostURL
+var user string = "super-api-1"
+var pass string = "b;Bn9RI]2HBh?M^."
+var host *string = &h
+var puser *string = &user
+var ppass *string = &pass
+
+type TLSconfig struct {
+	InsecureSkipVerify bool
+}
+
+func main() {
+	tlsconfig := &capam.TLSconfig{InsecureSkipVerify: false}
+	tlsconfig.InsecureSkipVerify = true
+	h, err := capam.NewClient(host, puser, ppass, tlsconfig)
+	if err != nil {
+		fmt.Println("PAM New Client Failed")
+		os.Exit(1)
+	}
+	var pvps []capam.PVP
+	pvps, err = h.GetPVPs()
+	if err != nil {
+		fmt.Println("GetPVPs Failed")
+		os.Exit(1)
+	}
+	s, _ := json.Marshal(pvps)
+	fmt.Println(string(s))
+}
